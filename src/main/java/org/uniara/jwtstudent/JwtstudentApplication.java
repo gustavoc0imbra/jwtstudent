74227be74a29a32fc3462cf0cc67736682c64a8f
Gustavo Coimbra <gustavocoimbra04@gmail.com>
2025-05-09 08:52:53 -0300

    Init project

---
+++ src/main/java/org/uniara/jwtstudent/JwtstudentApplication.java
@@ -0,0 +1,13 @@
+package org.uniara.jwtstudent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtstudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtstudentApplication.class, args);
+	}
+
+}
