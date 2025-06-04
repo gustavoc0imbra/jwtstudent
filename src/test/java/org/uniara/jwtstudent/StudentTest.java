07355104fc4714c0ae9cfc7d383540bf855a2c21
LeoZanardi <leonardo_fernando_zanardi@hotmail.com>
2025-06-04 19:54:08 -0300

    Teste

---
+++ src/test/java/org/uniara/jwtstudent/StudentTest.java
@@ -0,0 +1,14 @@
+package org.uniara.jwtstudent;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StudentTest {
+    @Test
+    public void test() {
+        Assertions.assertTrue(true);
+    }
+}
+
+
