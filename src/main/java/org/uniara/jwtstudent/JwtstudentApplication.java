6f28e6536dfc4ebcfb7b96d84bfce03c7a874867
Gustavo Coimbra <gustavocoimbra04@gmail.com>
2025-06-05 17:34:36 -0300

    Commit demonstrativo

--- src/main/java/org/uniara/jwtstudent/JwtstudentApplication.java
+++ src/main/java/org/uniara/jwtstudent/JwtstudentApplication.java
@@ -10,7 +10,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JwtstudentApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Olá Mundo");
 		SpringApplication.run(JwtstudentApplication.class, args);
 	}
 
